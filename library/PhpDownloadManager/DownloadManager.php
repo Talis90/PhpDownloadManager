f8a66b0bd03436fa6c7519fc2dbc533e9763f5dd
stephan <stephan@debian>
2015-01-12 01:51:39 +0100

    removed filemanager

--- library/PhpDownloadManager/DownloadManager.php
+++ library/PhpDownloadManager/DownloadManager.php
@@ -4,10 +4,35 @@ namespace PhpDownloadManager;
 
 class DownloadManager
 {
-	protected $fileManager;
+	protected static $translation = [
+		'en' => 'season',
+		'de' => 'staffel'
+	];
 	
-	function __construct(SeriesManager $seriesManager, $seriesBasePath) {
-		$this->fileManager = new FileManager($seriesManager, $seriesBasePath);
+	protected $seriesManager;
+	protected $seriesBasePath;
+	protected $language;
+	
+	function __construct(SeriesManager $seriesManager, $seriesBasePath, $language = 'en') {
+		$this->seriesManager = $seriesManager;
+		$this->seriesBasePath = $seriesBasePath;
+		$this->language = $language;
+	}
+	
+	function getLatestEpisode() {
+		return end(scandir($this->getSeriesPath() . DIRECTORY_SEPARATOR . $this->getLatestSeason()));
+	}
+	
+	function getLatestSeason() {
+// 		preg_match('/' . self::$translation[$this->language] . '\s[0-9]+/g', $subject, $matches);
+	}
+	
+	function getSeasons() {
+		return scandir($this->getSeriesPath());
+	}
+	
+	function getSeriesPath() {
+		return $this->seriesBasePath . DIRECTORY_SEPARATOR . $this->seriesManager->getSeriesName();
 	}
 	
 	function run() {
